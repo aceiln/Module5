19e35b5c73560101f3a9a483df5ba0c0f8ee766b
Ella <ellathitotran@gmail.com>
2023-07-19 20:23:28 -0500

    hi bf

--- 06-Stu_Form-Elements/Unsolved/assets/js/script.js
+++ 06-Stu_Form-Elements/Unsolved/assets/js/script.js
@@ -3,4 +3,16 @@ var shoppingListEl = $('#shopping-list');
 
 // TODO: Create a function to handle the form submission event that captures the form's `<input>` value and prints it to the `shoppingListEl` as a `<li>`
 
+function handleSubmit(event) {
+    event.preventDefault();
+    var input = $('#shopping-input');
+    var inputVal = input.val();
+
+    var listItemEl = $('<li>');
+    listItemEl.text(inputVal);
+    shoppingListEl.append(listItemEl);
+
+}
+
 // TODO: Add an event listener to the `shoppingFormEl` to handle submission
+shoppingFormEl.on('submit',handleSubmit);
